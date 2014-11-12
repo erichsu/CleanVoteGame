3ff6f79f32428e741644c11c96da22b476affaca
Eric Hsu <eric_hsu@livemail.tw>
2014-11-12 19:01:10 +0800

    Add Score.

--- Assets/Scripts/LienDControllerScript.cs
+++ Assets/Scripts/LienDControllerScript.cs
@@ -13,22 +13,22 @@ public class LienDControllerScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		speed = Random.Range (minSpeed, maxSpeed);
+//		speed = Random.Range (minSpeed, maxSpeed);
 	}
 
 	void FixedUpdate () {
 
-		rigidbody2D.MovePosition (new Vector2 (rigidbody2D.position.x + speed * Time.fixedDeltaTime, rigidbody2D.position.y));
-
-		if (maxPosX - rigidbody2D.position.x < speed * Time.fixedDeltaTime) {
-			facingLeft = false;
-			speed = Random.Range (minSpeed, maxSpeed);
-			speed *= facingLeft ? 1f : -1f;
-
-		} else if (rigidbody2D.position.x - minPosX < speed * Time.fixedDeltaTime) {
-			facingLeft = true;
-			speed = Random.Range (minSpeed, maxSpeed);
-			speed *= facingLeft ? 1f : -1f;
-		}
+//		rigidbody2D.MovePosition (new Vector2 (rigidbody2D.position.x + speed * Time.fixedDeltaTime, rigidbody2D.position.y));
+//
+//		if (maxPosX - rigidbody2D.position.x < speed * Time.fixedDeltaTime) {
+//			facingLeft = false;
+//			speed = Random.Range (minSpeed, maxSpeed);
+//			speed *= facingLeft ? 1f : -1f;
+//
+//		} else if (rigidbody2D.position.x - minPosX < speed * Time.fixedDeltaTime) {
+//			facingLeft = true;
+//			speed = Random.Range (minSpeed, maxSpeed);
+//			speed *= facingLeft ? 1f : -1f;
+//		}
 	}
 }
