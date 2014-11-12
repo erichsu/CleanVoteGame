919f6fb383559dec3ce0a13b7ed3d5f74570e23e
Eric Hsu <eric_hsu@livemail.tw>
2014-11-12 17:45:58 +0800

    Apply Ker P animation.

--- Assets/Scripts/KerPControllerScript.cs
+++ Assets/Scripts/KerPControllerScript.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class KerPControllerScript : MonoBehaviour {
 
-	float maxSpeed = 10f;
+	private float maxSpeed = 10f;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator> ();
 	}
 
 	void FixedUpdate () {
 		float move = Input.GetAxis ("Horizontal");
-		rigidbody2D.velocity = new Vector2 (maxSpeed * move, rigidbody2D.velocity.y);
+		float speed = maxSpeed * move;
+		rigidbody2D.velocity = new Vector2 (speed, rigidbody2D.velocity.y);
+
+		anim.SetFloat ("speed", speed);
 	}
 	
 	// Update is called once per frame
