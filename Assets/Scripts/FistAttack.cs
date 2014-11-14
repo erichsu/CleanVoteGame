ce011c1753d635198208c8dbb48ce4e15e3e4b00
Eric Hsu <eric_hsu@livemail.tw>
2014-11-14 18:32:29 +0800

    Add Attack button behavior.

---
+++ Assets/Scripts/FistAttack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FistAttack : MonoBehaviour {
+
+	private GameObject bound;
+	private BoundDetector boundDetector;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		bound = GameObject.Find ("Bound");
+		boundDetector = bound.GetComponent<BoundDetector> ();
+		anim = GameObject.Find ("Fist").GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1") && boundDetector.punchPoint == 100f)
+		{
+			// ... set the animator Shoot trigger parameter and play the audioclip.
+			
+			boundDetector.punchPoint = 0f;
+			anim.SetBool("FistReady", false);
+		}
+	}
+}
